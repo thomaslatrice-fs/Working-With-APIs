830ec98303151f9e2d9950cfd795c73c44ee8c80
thomaslatrice-fs <lathomas4@student.fullsail.edu>
2026-03-08 14:29:32 +0000

    basic HTML added

---
+++ js/main.js
@@ -0,0 +1,45 @@
+"use strict"
+/* Objective: Create an HTML interface that upon loading receives information from an API as well as updated via a button element in the HTML.
+
+Fetch API is required. Do NOT use async/await. Using. async/await could result in a zero grade.
+
+Key topics:
+
+Using fetch() to connect to an API
+Traversing objects to utilize data
+Arrow functions
+DOM Querying and Updates
+Button Click Events
+Overview:
+
+You will code an application that utilizes an API response through a Promise to update an interface via information stored in a JSON object.
+Assignment specifications:
+
+1. Create your own HTML
+
+Your HTML should be semantic and include at minimum a header, two sections for content, a footer, and any necessary tags for your dynamic content such as p, span, and headers.
+
+2. Create your own CSS
+
+Style your project to have an easy-to-use layout and a visual aesthetic you find fitting for the topic at hand. Don't forget to link a stylesheet to your HTML!
+
+3. Create your own JavaScript
+
+Fetch API is required. Do NOT use async/await. Using. async/await could result in a zero grade.
+
+Using the provided URL stated above, obtain data from the API that can be used to create an interactive page. The expectations for JavaScript are:
+
+fetch includes both then and catch methods
+Error handling for a rejected promise
+When the page first loads, the API is accessed and the setup and delivery of a joke are both present
+A reusable function for the API to be accessed through different events
+Clicking a "Load Another" button updates the interface with a new joke
+No HTML elements are created via JS. Please build your HTML to be querried by your JS using CSS classes or other identifiers
+Do not modify the API URL as this may lead to unexpected errors that are not part of this assessment
+All necessary JavaScript to run within an IFFE is organized as such.
+*/
+//IIFE with arrow function
+console.log("Demo Started");
+(()=>{
+
+})();
