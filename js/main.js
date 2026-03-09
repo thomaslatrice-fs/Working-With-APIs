ee7ac0781ed24ff94717453bdfa1696ed6a62df6
thomaslatrice-fs <lathomas4@student.fullsail.edu>
2026-03-09 03:08:24 +0000

    Finished code

--- js/main.js
+++ js/main.js
@@ -1,45 +1,39 @@
 "use strict"
-/* Objective: Create an HTML interface that upon loading receives information from an API as well as updated via a button element in the HTML.
 
-Fetch API is required. Do NOT use async/await. Using. async/await could result in a zero grade.
-
-Key topics:
-
-Using fetch() to connect to an API
-Traversing objects to utilize data
-Arrow functions
-DOM Querying and Updates
-Button Click Events
-Overview:
-
-You will code an application that utilizes an API response through a Promise to update an interface via information stored in a JSON object.
-Assignment specifications:
-
-1. Create your own HTML
-
-Your HTML should be semantic and include at minimum a header, two sections for content, a footer, and any necessary tags for your dynamic content such as p, span, and headers.
-
-2. Create your own CSS
-
-Style your project to have an easy-to-use layout and a visual aesthetic you find fitting for the topic at hand. Don't forget to link a stylesheet to your HTML!
-
-3. Create your own JavaScript
-
-Fetch API is required. Do NOT use async/await. Using. async/await could result in a zero grade.
-
-Using the provided URL stated above, obtain data from the API that can be used to create an interactive page. The expectations for JavaScript are:
-
-fetch includes both then and catch methods
-Error handling for a rejected promise
-When the page first loads, the API is accessed and the setup and delivery of a joke are both present
-A reusable function for the API to be accessed through different events
-Clicking a "Load Another" button updates the interface with a new joke
-No HTML elements are created via JS. Please build your HTML to be querried by your JS using CSS classes or other identifiers
-Do not modify the API URL as this may lead to unexpected errors that are not part of this assessment
-All necessary JavaScript to run within an IFFE is organized as such.
-*/
-//IIFE with arrow function
-console.log("Demo Started");
+//IIFE with arrow function runs code immediately
 (()=>{
-
+    //selects paragraph element where the joke will be displayed
+    const jokeText = document.querySelector("#jokeText");
+    //selects button element the user will click for new joke
+    const btn = document.querySelector("#btn");
+    //arrow function requests jokes from API
+    const getJoke = () => {
+        //requests data from joke API
+        fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart")
+        //converts response when received from JSON format    
+        .then((response) => {
+                return response.json(); 
+            })
+            //after JSON is ready access the joke
+            .then((data) => {
+                //store joke setup in a varible
+                const setup = data.setup;
+                //store punchline in varible
+                const delivery = data.delivery;
+                //display full joke on webpage
+                jokeText.textContent = setup + " ... " + delivery;
+            })
+            //displays error message if something goes wrong
+            .catch((error) => {
+                //show message if joke doesn't load
+                jokeText.textContent = "Jokes on you, something went wrong. Please try again";
+                console.log(error);//log error in console
+            });
+    };
+    //event to listen so new joke loads
+    btn.addEventListener("click", () => {
+        getJoke();
+    });
+    //call function so joke loads automatically when page loads
+    getJoke();
 })();
